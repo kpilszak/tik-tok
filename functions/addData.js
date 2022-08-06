ec824dcdc17f83d95b4742df47396730339b98fc
kpilszak <klaudia.pilszak@gmail.com>
2022-08-06 14:52:03 +0200

    Prepare connection to astra db

---
+++ functions/addData.js
@@ -0,0 +1,8 @@
+// const { createClient } = require("@astrajs/rest")
+
+// const astraClient = await createClient({
+//     astraDatabaseId: process.env.ASTRA_DB_ID,
+//     astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+//     username: process.env.ASTRA_DB_USERNAME,
+//     password: process.env.ASTRA_DB_PASSWORD,
+// })
