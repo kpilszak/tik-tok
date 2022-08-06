7fdf1297058f3f7a34c5c3779463e7db6c68d98d
kpilszak <klaudia.pilszak@gmail.com>
2022-08-06 13:34:36 +0200

    Add styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+import Header from './components/Header'
+import './App.css'
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path='/upload' element={<Upload/>} />
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
